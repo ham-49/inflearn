4e3efa39ef56e54870763ffa3c04f0b5b21dab9b
ham-49 <hamoon0224@gmail.com>
2025-06-23 14:17:22 +0000

    Update swiper.js

--- script/swiper.js
+++ script/swiper.js
@@ -1,38 +1,24 @@
-if (matchMedia("screen and (max-width: 480px)").matches) {
-  console.log("mobile");
-  var swiper = new Swiper(".mySwiper02", {
-    slidesPerView: 2, // 한번에 보여줄 슬라이드 수
-    spaceBetween: 30, // 간격
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: true,
-      pauseOnMouseEnter: true,
+var swiper = new Swiper(".mySwiper02", {
+  slidesPerView: 2, // 기본값: 모바일 기준
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+    pauseOnMouseEnter: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    481: {
+      slidesPerView: 3,
+      spaceBetween: 25,
     },
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30,
     },
-  });
-} else if (matchMedia("screen and (min-width: 768px)").matches) {
-  console.log("desktop");
-  var swiper = new Swiper(".mySwiper02", {
-    slidesPerView: 4, // 한번에 보여줄 슬라이드 수
-    spaceBetween: 30, // 간격
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: true,
-      pauseOnMouseEnter: true,
-    },
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-}
-
-/* 크기 변할때 자동 새로고침 */
-window.onresize = function(){
-  document.location.reload();
-};
+  },
+});
