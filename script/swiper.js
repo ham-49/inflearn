aac4f2b8dee6568eb30af06a6a5a87eeabcfb410
ham-49 <hamoon0224@gmail.com>
2025-06-23 17:33:04 +0900

    Update swiper.js

--- script/swiper.js
+++ script/swiper.js
@@ -4,19 +4,8 @@ var swiper = new Swiper(".mySwiper", {});
         spaceBetween:30, //간격
         autoplay:{
           delay:3000, 
-          disableOnInteraction	:true, 
+          disableOnInteraction:true, 
           pauseOnMouseEnter: true,
-        },
-        breakpoints:{
-          768:{
-            slidesPerView:3,
-          },
-          480:{
-            slidesPerView:2,
-          },
-          300:{
-            slidesPerView:2,
-          },
         },
         loop:true,
         navigation: {
@@ -24,3 +13,43 @@ var swiper = new Swiper(".mySwiper", {});
           prevEl: ".swiper-button-prev",
         },
       });
+if(matchMedia("screen and (max-width: 480px)").matches){
+  console.log("mobile");
+ var swiper = new Swiper(".mySwiper", {});
+      var swiper = new Swiper(".mySwiper02", {
+        slidesPerView:2, //한번에 보여줄 슬라이드 수
+        spaceBetween:30, //간격
+        autoplay:{
+          delay:3000, 
+          disableOnInteraction:true, 
+          pauseOnMouseEnter: true,
+        },
+        loop:true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+}else if(matchMedia("screen and (min-width: 768px)").matches){
+  console.log("desktop");
+  var swiper = new Swiper(".mySwiper", {});
+      var swiper = new Swiper(".mySwiper02", {
+        slidesPerView:4, //한번에 보여줄 슬라이드 수
+        spaceBetween:30, //간격
+        autoplay:{
+          delay:3000, 
+          disableOnInteraction:true, 
+          pauseOnMouseEnter: true,
+        },
+        loop:true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+  });
+};
+/* 크기 변할때 자동 새로고침 */
+window.onresize = function(){
+  document.location.reload();
+};
