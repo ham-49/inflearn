3e7f0668e35c73ed762d40ab45d489b044a98734
ham-49 <hamoon0224@gmail.com>
2025-06-23 17:34:56 +0900

    Update swiper.js

--- script/swiper.js
+++ script/swiper.js
@@ -1,18 +1,3 @@
-var swiper = new Swiper(".mySwiper", {});
-      var swiper = new Swiper(".mySwiper02", {
-        slidesPerView:4, //한번에 보여줄 슬라이드 수
-        spaceBetween:30, //간격
-        autoplay:{
-          delay:3000, 
-          disableOnInteraction:true, 
-          pauseOnMouseEnter: true,
-        },
-        loop:true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
 if(matchMedia("screen and (max-width: 480px)").matches){
   console.log("mobile");
  var swiper = new Swiper(".mySwiper", {});
